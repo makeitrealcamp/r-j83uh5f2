f6ff5272c0a9dada8dc0b444a073e143bc8c17b8
German Escobar <german.escobarc@gmail.com>
2016-11-29 02:42:19 -0500

    Implement the challenge

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it('renders the textarea', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('textarea').length).toBe(1);
+});
+
+it('renders the counter', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.counter').length).toBe(1);
+  expect(wrapper.find('.counter').text()).toBe("0");
+});
+
+it('updates the counter', () => {
+  const wrapper = shallow(<App />);
+  wrapper.find('textarea').simulate('change', { target: { value: 'Hola Mundo' } });
+  expect(wrapper.find('.counter').text()).toBe("10");
 });
